74a77d16146b7514191ef1b9c32330a885c6ffb3
Vincent <schuhmacher@jaimo-solutions.com>
2026-04-19 13:00:32 +0200

    bump

--- build_push.sh
+++ build_push.sh
@@ -1,2 +1,2 @@
-docker build -t jakeiolipopp/runpod-docker-builder:0.8.2 .
-docker push jakeiolipopp/runpod-docker-builder:0.8.2
+docker build -t jakeiolipopp/runpod-docker-builder:0.8.3 .
+docker push jakeiolipopp/runpod-docker-builder:0.8.3
